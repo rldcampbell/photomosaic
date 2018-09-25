54b763e12d2b991c0eb7a469765ea971b4d2d023
Robert Campbell <rldcampbell@gmail.com>
2018-09-25 01:16:00 +0100

    removed unnecessary metadata call

--- src/tiles.js
+++ src/tiles.js
@@ -2,48 +2,44 @@ const sharp = require("sharp")
 const buffer = require("./buffer.js")
 
 const fromPath = function(filePath, shape) {
-  return sharp(filePath)
-    .metadata()
-    .then(info => {
-      const step = 10
-      const dims = shape.map(v => step * v)
+  const step = 10
+  const dims = shape.map(v => step * v)
 
-      const resized = sharp(filePath)
-        .rotate()
-        .resize(...dims)
+  const resized = sharp(filePath)
+    .rotate()
+    .resize(...dims)
 
-      const tiles = []
+  const tiles = []
 
-      for (let i = 0; i < shape[0]; i++) {
-        for (let j = 0; j < shape[1]; j++) {
-          tiles[i * shape[1] + j] = resized
-            .clone()
-            .extract({
-              width: step,
-              height: step,
-              left: i * step,
-              top: j * step
-            })
-            .raw()
-            .toBuffer({ resolveWithObject: true })
-            .then(({ data, info }) => {
-              let rgb = buffer[info.channels === 3 ? "meanRGB" : "meanRGBA"](
-                data
-              ).slice(0, 3)
-              return {
-                i,
-                j,
-                data,
-                rgb,
-                brightness: rgb.reduce((p, c) => p + c / 3, 0),
-                channels: info.channels
-              }
-            })
-        }
-      }
+  for (let i = 0; i < shape[0]; i++) {
+    for (let j = 0; j < shape[1]; j++) {
+      tiles[i * shape[1] + j] = resized
+        .clone()
+        .extract({
+          width: step,
+          height: step,
+          left: i * step,
+          top: j * step
+        })
+        .raw()
+        .toBuffer({ resolveWithObject: true })
+        .then(({ data, info }) => {
+          let rgb = buffer[info.channels === 3 ? "meanRGB" : "meanRGBA"](
+            data
+          ).slice(0, 3)
+          return {
+            i,
+            j,
+            data,
+            rgb,
+            brightness: rgb.reduce((p, c) => p + c / 3, 0),
+            channels: info.channels
+          }
+        })
+    }
+  }
 
-      return Promise.all(tiles)
-    })
+  return Promise.all(tiles)
 }
 
 exports.fromPath = fromPath
